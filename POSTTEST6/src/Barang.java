8315995be25224ea5432ee3cfd6d5688bda0f2c9
Agung714 <113093617+Agung714@users.noreply.github.com>
2024-04-29 13:12:54 +0800

    Delete POSTTEST6 directory

--- POSTTEST6/src/Barang.java
+++
@@ -1,8 +0,0 @@
-// package src;
-import java.util.*;
-public interface Barang {
-    // public static ArrayList<Barang> DataBarang = new ArrayList<>();;
-    
-    public void tampil_data() ;
-    }
-
